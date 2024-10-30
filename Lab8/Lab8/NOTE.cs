3b803cdf3e44d4181b3c4b37e47bae4ab65d3e52
KkolyadkoO <artem.7ko52@gmail.com>
2024-10-31 01:14:17 +0300

    Lab8

---
+++ Lab8/Lab8/NOTE.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab8;
+
+public struct NOTE
+{
+    public string FullName  { get; set; }
+    public string PhoneNumber { get; set; }
+    public int[] BirthDate { get; set; } 
+
+    public NOTE(string fullName, string phoneNumber, int[] birthDate)
+    {
+        FullName = fullName;
+        PhoneNumber = phoneNumber;
+        BirthDate = birthDate;
+    }
+
+    public override string ToString()
+    {
+        return $"{FullName}, {PhoneNumber}, {BirthDate[0]}/{BirthDate[1]}/{BirthDate[2]}";
+    }
+}
